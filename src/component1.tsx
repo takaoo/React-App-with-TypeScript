37a66ec72b3b486dd6579924f47931b586284745
takaoo <hatopopppoooo@gmail.com>
2020-01-11 09:48:04 +0900

    Add files via upload

---
+++ src/component1.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Component1: React.FC = () => {
+  return (
+    <div>
+      {random()}
+    </div>
+  );
+}
+export default Component1;
+
+
+const random = (): number => {
+  let random = 0;
+
+  let arr: number[] = []
+
+  for (let index = 0; index < 4; index++) {
+    arr.push(index + 1);
+    console.log(arr);
+  }
+
+  for (let index = 0; index < arr.length; index++) {
+    random += arr[index] * 10 ** (arr.length - index - 1)
+
+    // console.log(arr[index], 10 ** (arr.length - index));
+
+    console.log(random);
+
+  }
+  return random
+}
