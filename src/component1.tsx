1e2cb0f20d6725e25a97064de03be077cdbb907e
takaoo <hatopopppoooo@gmail.com>
2020-01-17 12:26:05 +0900

    Add files via upload

--- src/component1.tsx
+++ src/component1.tsx
@@ -1,33 +1,125 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core';
+import Input from '@material-ui/core/Input';
+import Paper from '@material-ui/core/Paper';
+
+let inputValue = 0;
+let A = false;
 
 const Component1: React.FC = () => {
-  return (
-    <div>
-      <h1>Component1</h1>
-      {random()}
-    </div>
-  );
-}
+    const classes = useStyles();
+    const [num, setNum] = React.useState(0);
+    const [result, setResult] = React.useState('');
+    return (
+        <div>
+            <Paper>
+                <h1>Component1</h1>
+                <h2>Please select number</h2>
+                <div>
+                    <Button
+                        disabled={A}
+                        variant="contained"
+                        onClick={e => {
+                            setNum(gen_random(4));
+                        }}
+                    >
+                        4
+                    </Button>
+                    {/* </div>
+            <div> */}
+                    <Button
+                        disabled={A}
+                        variant="contained"
+                        onClick={e => {
+                            setNum(gen_random(5));
+                        }}
+                    >
+                        5
+                    </Button>
+                    {/* </div>
+            <div> */}
+                    <Button
+                        disabled={A}
+                        variant="contained"
+                        onClick={e => {
+                            setNum(gen_random(6));
+                        }}
+                    >
+                        6
+                    </Button>
+                </div>
+                <div className={classes.root}>{num}</div>
+                <Input
+                    className={classes.input}
+                    type="number"
+                    onChange={event => {
+                        inputValue = Number(event.target.value);
+                        console.log(event.target.value);
+                    }}
+                />
+                <Button
+                    onClick={e => {
+                        console.log('正解は', num);
+                        console.log('inputValueは', inputValue);
+                        setResult(check(num));
+                    }}
+                >
+                    check
+                </Button>
+            </Paper>
+            result: {result}
+        </div>
+    );
+};
+
 export default Component1;
 
+const gen_random = (n: number): number => {
+    let randoms: number[] = [];
+    let random = 0;
+    for (let i = 1; i <= n; i++) {
+        while (true) {
+            var tmp = Math.floor(Math.random() * 9) + 1;
+            if (!randoms.includes(tmp)) {
+                randoms.push(tmp);
+                random += tmp * 10 ** (n - i);
+                break;
+            }
+        }
+    }
+    A = true;
+    return random;
+};
 
-const random = (): number => {
-  let random = 0;
-
-  let arr: number[] = []
-
-  for (let index = 0; index < 4; index++) {
-    arr.push(index + 1);
+const check = (num: number) => {
+    let Hits = 0;
+    let Blow = 0;
+    let arr = String(num).split('');
     console.log(arr);
-  }
 
-  for (let index = 0; index < arr.length; index++) {
-    random += arr[index] * 10 ** (arr.length - index - 1)
+    let strinput = inputValue.toString();
+    for (let i = 0; i < arr.length; i++) {
+        console.log(strinput.indexOf(arr[i]));
+        if (strinput.indexOf(arr[i]) === i) {
+            //同じ場所で同じ数字
+            Hits++;
+        } else if (strinput.indexOf(arr[i]) === -1) {
+            //はずれ
+        } else {
+            //かすり
+            Blow++;
+        }
+    }
 
-    // console.log(arr[index], 10 ** (arr.length - index));
-
-    console.log(random);
-
-  }
-  return random
-}
+    return 'Hits: ' + Hits + ' Blow: ' + Blow;
+};
+const useStyles = makeStyles({
+    root: {
+        fontSize: '2em'
+        // color: 'blue'
+    },
+    input: {
+        fontSize: '4em'
+    }
+});
